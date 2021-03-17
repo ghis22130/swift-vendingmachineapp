17180e7ccbbfba9a05363fa74d4e648bdf53d6aa
Jisoo Kim <ghis22130@naver.com>
2021-03-17 16:05:02 +0900

    Dictionary 수정

--- VendingMachineApp/VendingMachineApp/Models/BeverageInventory.swift
+++ VendingMachineApp/VendingMachineApp/Models/BeverageInventory.swift
@@ -7,155 +7,115 @@
 
 import Foundation
 
-typealias Inventory = Dictionary<ObjectIdentifier, [Beverage]>
-
 class BeverageInventory: NSObject, NSCoding {
-    
-    private var allTypes: [Beverage.Type]
-    private (set) var beverageInventory: Inventory
-    
-    override init(){
-        self.allTypes = [StrawberryMilk.self, ChocoMilk.self, Sprite.self, CocaCola.self, Top.self, Cantata.self]
-        self.beverageInventory = [:]
-        super.init()
-        setting()
-    }
-    
-    func encode(with coder: NSCoder) {
-        coder.encode(allTypes, forKey: "allTypes")
-        coder.encode(allBeverages(), forKey: "allBeverages")
-    }
-    
-    required init?(coder: NSCoder) {
-        self.allTypes = coder.decodeObject(forKey: "allTypes") as! [Beverage.Type]
-        let allBeverages = coder.decodeObject(forKey: "allBeverages") as! [[Beverage]]
-        var beverageInventory = Inventory()
-        
-        allTypes.forEach {
-            beverageInventory[ObjectIdentifier($0)] = []
-        }
-        
-        allBeverages.forEach {
-            beverageInventory[ObjectIdentifier(type(of: $0[0]))]! = $0
-        }
-        self.beverageInventory = beverageInventory
-        
-    }
-    
-    private func setting() {
-        allTypes.forEach {
-            beverageInventory[ObjectIdentifier($0)] = []
-        }
-    }
-    
-    func append(_ beverage: Beverage) {
-        let drinkType = ObjectIdentifier(type(of: beverage))
-        beverageInventory[drinkType]?.append(beverage)
-    }
-    
-    func append(_ beverages: [Beverage]) {
-        beverages.forEach { beverage in
-            let drinkType = ObjectIdentifier(type(of: beverage))
-            beverageInventory[drinkType]?.append(beverage)
-        }
-    }
-    
-    func hasBeverage(with beverage: Beverage) ->  Bool {
-        let beverageType = ObjectIdentifier(type(of: beverage))
-        if beverageInventory[beverageType]?.count == 0 {
-            return false
-        } else {
-            return true
-        }
-    }
-    
-    func show(handler: (Beverage) -> Void) {
-        beverageInventory.values.forEach {
-            $0.forEach {
-                handler($0)
-            }
-        }
-    }
-    
-    func pop(_ drink: Beverage) -> Beverage? {
-        let drinkType = ObjectIdentifier(type(of: drink))
-        
-        if beverageInventory[drinkType] != nil && hasBeverage(with: drink) {
-            return beverageInventory[drinkType]!.remove(at: 0)
-        }
-        return nil
-    }
-    
-    func count() -> Int {
-        beverageInventory.count
-    }
-    
-    func countType() -> Int {
-        return allTypes.count
-    }
-    
-    func beverageType(at index: Int) -> Beverage.Type {
-        return allTypes[index]
-    }
-    
-    func countBeverage(at index: Int) -> Int {
-        let beverageType = ObjectIdentifier(allTypes[index])
-        return beverageInventory[beverageType]?.count ?? 0
-    }
-    
-    func allBeverages() -> [[Beverage]] {
-        var beverages: [[Beverage]] = []
-        beverageInventory.values.forEach {
-            beverages.append($0)
-        }
-        return beverages
-    }
-    
-    func possibleBeverages(with credit: Money) -> [Beverage]{
-        var beverages:[Beverage] = []
-        beverageInventory.values.forEach {
-            $0.forEach {
-                if $0.canBuy(with: credit) {
-                    beverages.append($0)
-                }
-            }
-        }
-        return beverages
-    }
-    
-    func hotBeverages() -> [Beverage] {
-        var beverages: [Beverage] = []
-        beverageInventory.values.forEach {
-            $0.forEach {
-                if $0.isHot() {
-                    beverages.append($0)
-                }
-            }
-        }
-        return beverages
-    }
-    
-    func lowCalorieBeverages() -> [Beverage] {
-        var beverages: [Beverage] = []
-        beverageInventory.values.forEach {
-            $0.forEach {
-                if $0.isLowCalore() {
-                    beverages.append($0)
-                }
-            }
-        }
-        return beverages
-    }
-    
-    func validateBeverages(when date: Date) -> [Beverage] {
-        var beverages: [Beverage] = []
-        beverageInventory.values.forEach {
-            $0.forEach {
-                if $0.validate(date: date) {
-                    beverages.append($0)
-                }
-            }
-        }
-        return beverages
-    }
+     
+     private var allTypes: [Beverage.Type]
+     private (set) var beverages: [Beverage]
+     
+     override init(){
+          self.allTypes = [StrawberryMilk.self, ChocoMilk.self, Sprite.self, CocaCola.self, Top.self, Cantata.self]
+          self.beverages = []
+          super.init()
+     }
+     
+     func encode(with coder: NSCoder) {
+          coder.encode(allTypes, forKey: "allTypes")
+          coder.encode(beverages, forKey: "beverages")
+     }
+     
+     required init?(coder: NSCoder) {
+          self.allTypes = coder.decodeObject(forKey: "allTypes") as! [Beverage.Type]
+          self.beverages = coder.decodeObject(forKey: "beverages") as! [Beverage]
+     }
+     
+     func append(_ beverage: Beverage) {
+          beverages.append(beverage)
+     }
+     
+     func append(_ beverages: [Beverage]) {
+          beverages.forEach {
+               self.beverages.append($0)
+          }
+     }
+     
+     func show(handler: (Beverage) -> Void) {
+          beverages.forEach {
+               $0.forEach {
+                    handler($0)
+               }
+          }
+     }
+     
+     func pop(_ beverage: Beverage) {
+          if let index = beverages.firstIndex(where: {ObjectIdentifier(type(of: $0)) == ObjectIdentifier(beverage)}) {
+               beverages.remove(at: index)
+          }
+     }
+     
+     func count() -> Int {
+          beverages.count
+     }
+     
+     func countType() -> Int {
+          return allTypes.count
+     }
+     
+     func beverageType(at index: Int) -> Beverage.Type {
+          return allTypes[index]
+     }
+     
+     func countBeverage(at index: Int) -> Int {
+          var count = 0
+          beverages.forEach {
+               if ObjectIdentifier(type(of: $0)) == ObjectIdentifier(allTypes[index]) {
+                    count += 1
+               }
+          }
+          return count
+     }
+     
+     func allBeverages() -> Dictionary<ObjectIdentifier, [Beverage]> {
+          var inventory: Dictionary<ObjectIdentifier, [Beverage]> = [:]
+          inventory = beverages.reduce(into: [:]) { dict, beverage in
+               let objectIdentifier = ObjectIdentifier(type(of: beverage))
+               dict[objectIdentifier, default: []].append(beverage)
+          }
+          return inventory
+     }
+     
+     func possibleBeverages(with credit: Money) -> [Beverage]{
+          var beverages:[Beverage] = []
+          beverageInventory.values.forEach {
+               $0.forEach {
+                    if $0.canBuy(with: credit) {
+                         beverages.append($0)
+                    }
+               }
+          }
+          return beverages
+     }
+     
+     func hotBeverages(unit: Double) -> [Beverage] {
+          var hotBeverages: [Beverage] = []
+          hotBeverages = beverages.filter {
+               $0.isHot(unit: unit)
+          }
+          return hotBeverages
+     }
+     
+     func lowCalorieBeverages(unit: Int) -> [Beverage] {
+          var lowCalorieBeverages: [Beverage] = []
+          lowCalorieBeverages = beverages.filter {
+               $0.isLowCalore(unit: unit)
+          }
+          return lowCalorieBeverages
+     }
+     
+     func validateFromExpired(when date: Date) -> [Beverage] {
+          var validateBeverages: [Beverage] = []
+          validateBeverages = beverages.filter {
+               $0.validateFromExpired(date: date)
+          }
+          return validateBeverages
+     }
 }
